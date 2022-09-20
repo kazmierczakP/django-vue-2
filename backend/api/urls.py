3eef0d6e7a9836b6305320d8a8f6f1cbf211d382
Kaźmierczak Patryk <Patryk.Kazmierczak@t-mobile.pl>
2022-09-20 16:14:52 +0200

    switched from APIView to ViewSet & from path to routers

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,15 @@
+'''
 from django.urls import path
 from . import views
 
 urlpatterns=[
     path('students', views.StudentApi.as_view()),
-] 
+]
+'''
+
+from rest_framework import routers
+from . import views
+
+router = routers.DefaultRouter()
+router.register('students', views.StudentsViewSet, basename='Students')
+urlpatterns = router.urls
