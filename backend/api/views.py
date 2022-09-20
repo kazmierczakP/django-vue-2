adbb4eb4df948b51d6a28de5a9d436ea69c80d30
Kaźmierczak Patryk <Patryk.Kazmierczak@t-mobile.pl>
2022-09-20 13:48:52 +0200

    setting up api app

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
 # Create your views here.
+
+class StudentApi(APIView):
+    def get(self, request):
+        students = []
+        return Response({'msg': students})
